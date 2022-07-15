4dc021413b258e4de22858b344a4b6da5ffa5022
DTk0cz <Damian.Tkocz@outlook.com>
2022-07-15 15:55:58 +0200

    Add project files.

---
+++ CookBook/Program.cs
@@ -0,0 +1,69 @@
+﻿using CookBook;
+
+MenuActionService actionService = new MenuActionService();
+RecipeService recipeService = new RecipeService();
+actionService = Initialize(actionService);
+
+Console.WriteLine("Welcome to the cookbook app!");
+
+while (true)
+{
+
+
+
+    Console.WriteLine("Please tell me what you want to do: ");
+
+
+    var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+    for (int i = 0; i < mainMenu.Count; i++)
+    {
+        Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+    }
+
+    var operation = Console.ReadKey();
+    
+    switch (operation.KeyChar)
+    {
+        case '1':
+            var keyInfo = recipeService.AddNewItemView(actionService);
+            var id = recipeService.AddNewItem(keyInfo.KeyChar);
+            break;
+        case '2':
+            var removeId = recipeService.RemoveRecipeView();
+            recipeService.RemoveRecipe(removeId);
+            break;
+        case '3':
+            var detailId = recipeService.RecipeDetailSelectionView();
+            recipeService.RecipeDetailView(detailId);
+            break;
+        case '4':
+            var typeId = recipeService.RecipeTypeSelectionView();
+            recipeService.RecipesByTypeIdView(typeId);
+            break;
+        default:
+            Console.WriteLine("Actioned you entered does not exist");
+            break;
+
+    }
+
+}
+
+
+
+
+
+
+static MenuActionService Initialize(MenuActionService actionService)
+{
+    actionService.AddNewAction(1, "Add recipe", "Main");
+    actionService.AddNewAction(2, "Remove recipe", "Main");
+    actionService.AddNewAction(3, "Show details", "Main");
+    actionService.AddNewAction(4, "Show all recipes", "Main");
+
+    actionService.AddNewAction(1, "Breakfeast", "AddNewRecipeMenu");
+    actionService.AddNewAction(2, "Dinner", "AddNewRecipeMenu");
+    actionService.AddNewAction(3, "Supper", "AddNewRecipeMenu");
+    
+
+    return actionService;
+}
---
+++ CookBook/RecipeService.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook
+{
+    internal class RecipeService
+    {
+        public List<Recipe> Recipes { get; set; }
+        public RecipeService()
+        {
+            Recipes = new List<Recipe>();
+        }
+
+        public ConsoleKeyInfo AddNewItemView(MenuActionService actionService)
+        {
+            var addNewItemMenu = actionService.GetMenuActionsByMenuName("AddNewRecipeMenu");
+            Console.WriteLine("Please select recipe type:");
+            for(int i = 0; i < addNewItemMenu.Count; i++)
+            {
+                Console.WriteLine($"{addNewItemMenu[i].Id}. {addNewItemMenu[i].Name}");
+            }
+
+            var operation = Console.ReadKey();
+            return operation;
+        }
+
+        public int AddNewItem(char recipeType)
+        {
+            int recipeTypeId;
+            Int32.TryParse(recipeType.ToString(), out recipeTypeId);
+            Recipe recipe = new Recipe();
+            recipe.TypeId = recipeTypeId;
+            Console.WriteLine("Please enter id for new recipe:");
+            var id = Console.ReadLine();
+            int recipeId;
+            Int32.TryParse(id, out recipeId);
+            Console.WriteLine("Please enter name for new item:");
+            var name = Console.ReadLine();
+
+            recipe.Id = recipeId;
+            recipe.Name = name;
+            recipe.TypeId = recipeTypeId;
+
+            Recipes.Add(recipe);
+            return recipeId;
+
+            
+        }
+
+        public int RemoveRecipeView()
+        {
+            Console.WriteLine("Please enter id for recipe you want to remove:");
+            var recipeId = Console.ReadKey();
+            int id;
+            Int32.TryParse(recipeId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+        public void RemoveRecipe(int removeId)
+        {
+            Recipe recipeToRemove = new Recipe();
+            foreach(var recipe in Recipes)
+            {
+                if(recipe.Id == removeId)
+                {
+                    recipeToRemove = recipe;
+                    break;
+                }
+            }
+
+            Recipes.Remove(recipeToRemove);
+        }
+
+        public int RecipeTypeSelectionView()
+        {
+            Console.WriteLine("Please enter Type id for items you want to show:");
+            var recipeId = Console.ReadKey();
+            int id;
+            Int32.TryParse(recipeId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+        public void RecipesByTypeIdView(int typeId)
+        {
+            List<Recipe> toShow = new List<Recipe>();
+            foreach(var recipe in Recipes)
+            {
+                if(recipe.TypeId == typeId)
+                {
+                    toShow.Add(recipe);
+                }
+            }
+
+            for(int i = 0; i < toShow.Count; i++)
+            {
+                Console.WriteLine($"Id {toShow[i].Id}, Name {toShow[i].Name}");
+            }
+        }
+
+        public void RecipeDetailView(int detailId)
+        {
+            Recipe recipeToShow = new Recipe();
+            foreach(var recipe in Recipes)
+            {
+                if(recipe.Id == detailId)
+                {
+                    recipeToShow = recipe;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Recipe id: {recipeToShow.Id}");
+            Console.WriteLine($"Recipe name: {recipeToShow.Name}");
+            Console.WriteLine($"Recipe type: {recipeToShow.TypeId}");
+        }
+
+        public int RecipeDetailSelectionView()
+        {
+            Console.WriteLine("Please enter id for recipe you want to show: ");
+            var recipeId = Console.ReadKey();
+            int id;
+            Int32.TryParse(recipeId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+    }
+}
