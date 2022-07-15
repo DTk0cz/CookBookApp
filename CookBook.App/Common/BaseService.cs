c48ac31ef235df6eed69affe16b62666fd67c4ad
DTk0cz <Damian.Tkocz@outlook.com>
2022-07-15 17:40:06 +0200

    Commit

---
+++ CookBook.App/Common/BaseService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CookBook.App.Abstract;
+
+namespace CookBook.App.Common
+{
+    public class BaseService<T> : IService<T>
+    {
+        public List<T> Recipes { get; set; }
+
+        public BaseService()
+        {
+            Recipes = new List<T>();
+        }
+
+        public int AddRecipe(T recipe)
+        {
+            Recipes.Add(recipe);
+            return recipe.Id;
+        }
+
+        public List<T> GetAllRecipes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRecipe(T recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateRecipe(T recipe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
