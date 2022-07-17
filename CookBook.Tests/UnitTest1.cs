312e5e91d9c28cd72d12d3688e3f4c3be5635ccf
DTk0cz <Damian.Tkocz@outlook.com>
2022-07-17 18:28:42 +0200

    Tests

--- CookBook.Tests/UnitTest1.cs
+++ CookBook.Tests/UnitTest1.cs
@@ -9,7 +9,15 @@ namespace CookBook.Tests
         [Fact]
         public void Test1()
         {
+            //Arrange
+            int a = 5;
+            int b = 3;
+            //Act
+            int result = a + b;
+            //Assert
+            Assert.Equal(8, result);
 
+           
         }
     }
 }
